21360ef2d4137806ccb61ae6394ea230c8cfe4ba
Saksham Gupta <www.rameshgupta67@gmail.com>
2023-06-19 00:51:05 +0530

    fixed issues in the data_loader function

--- data_loader.py
+++ data_loader.py
@@ -3,24 +3,30 @@ import numpy as np
 import cv2
 
 def load_dataset(path):
-
     images = []
     labels = []
 
     for filename in os.listdir(path):
-        if filename.startswith("cat") and filename.endswith(".jpg"):
-            img = cv2.imread(os.path.join(path, filename))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img, (224, 224))
-            images.append(img)
-            labels.append(1)
-
+        if filename.startswith("cat"):
+            img_path = os.path.join(path, filename)
+            img = cv2.imread(img_path)
+            if img is not None:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img = cv2.resize(img, (224, 224))
+                images.append(img)
+                labels.append(1)
+            else:
+                print('Failed to load image:', img_path)
         else:
-            img = cv2.imread(os.path.join(path, filename))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img, (224, 224))
-            images.append(img)
-            labels.append(0)
+            img_path = os.path.join(path, filename)
+            img = cv2.imread(img_path)
+            if img is not None:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img = cv2.resize(img, (224, 224))
+                images.append(img)
+                labels.append(0)
+            else:
+                print('Failed to load image:', img_path)
 
     images = np.array(images)
     labels = np.array(labels)
